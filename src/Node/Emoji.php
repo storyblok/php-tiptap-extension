4403ed35991a52eb9cda75f07bf39677dd0e8aa9
Silas Joisten <silasjoisten@proton.me>
2025-07-22 07:07:07 +0300

    fix

--- src/Node/Emoji.php
+++ src/Node/Emoji.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of Storyblok PHP Tiptap Extension.
+ *
+ * (c) Storyblok GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Storyblok\Tiptap\Node;
 
 use Tiptap\Core\Node;
@@ -10,7 +19,12 @@ final class Emoji extends Node
 {
     public static $name = 'emoji';
 
-    public function renderHTML($node)
+    /**
+     * @param \stdClass $node
+     *
+     * @return array<string, mixed>
+     */
+    public function renderHTML($node): array
     {
         if (!isset($node->attrs) || !isset($node->attrs->emoji)) {
             return [];
