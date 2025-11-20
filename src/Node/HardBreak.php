a0339b3c9d10c71b487ccb1d71a298bdf5ccf09f
Silas Joisten <silasjoisten@proton.me>
2025-11-20 13:49:06 +0200

    fix: `hard_break` name

---
+++ src/Node/HardBreak.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Storyblok PHP Tiptap Extension.
+ *
+ * (c) Storyblok GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Storyblok\Tiptap\Node;
+
+use Tiptap\Nodes\HardBreak as BaseHardBreak;
+
+final class HardBreak extends BaseHardBreak
+{
+    public static $name = 'hard_break';
+}
